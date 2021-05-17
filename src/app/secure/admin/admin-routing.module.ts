4f6e49cbcb27c5f1b8fe6fb95a829209c7313f92
mohamed ayadi <mamayadi@gmail.com>
2021-05-17 23:02:09 +0100

    Add etudiant CRUD

--- src/app/secure/admin/admin-routing.module.ts
+++ src/app/secure/admin/admin-routing.module.ts
@@ -1,9 +1,11 @@
 import { RouterModule, Routes } from '@angular/router';
 
 import { AddAdminComponent } from './admin-list/add-admin/add-admin.component';
+import { AddEtudiantComponent } from './etudiant-list/add-etudiant/add-etudiant.component';
 import { AdminComponent } from './admin.component';
 import { AdminListComponent } from './admin-list/admin-list.component';
 import { EditAdminComponent } from './admin-list/edit-admin/edit-admin.component';
+import { EditEtudiantComponent } from './etudiant-list/edit-etudiant/edit-etudiant.component';
 import { EtudiantListComponent } from './etudiant-list/etudiant-list.component';
 import { FormateurListComponent } from './formateur-list/formateur-list.component';
 
@@ -22,6 +24,8 @@ const routes: Routes = [
       { path: 'admin-list/edit/:id', component: EditAdminComponent },
       { path: 'formateur-list', component: FormateurListComponent },
       { path: 'etudiant-list', component: EtudiantListComponent },
+      { path: 'etudiant-list/add', component: AddEtudiantComponent },
+      { path: 'etudiant-list/edit/:id', component: EditEtudiantComponent },
       {
         path: '**',
         redirectTo: '/404',
---
+++ src/app/secure/admin/etudiant-list/edit-etudiant/edit-etudiant.component.ts
@@ -0,0 +1,89 @@
+import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { Etudiant } from 'src/app/shared/models/Etudiant.model';
+import { EtudiantService } from 'src/app/secure/services/etudiant.service';
+import { Location } from '@angular/common';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-edit-etudiant',
+  templateUrl: './edit-etudiant.component.html',
+  styleUrls: ['./edit-etudiant.component.scss']
+})
+export class EditEtudiantComponent implements OnInit {
+  etudiant$: Observable<Etudiant>;
+  etudiant: Etudiant;
+  etudiantForm: FormGroup;
+
+  constructor(
+    private snackBar: MatSnackBar,
+    private fb: FormBuilder,
+    private etudiantService: EtudiantService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.etudiant$ = this.etudiantService.getEtudiant(params.id);
+    });
+    this.etudiant$?.subscribe(
+      (etudiant) => {
+        this.etudiant = etudiant;
+        this.etudiantForm = this.createForm(etudiant);
+      },
+      (error) => {
+        this.snackBar.open('Etudiant introuvable!', 'OK', {
+          duration: 3500,
+          panelClass: ['mat-toolbar', 'mat-primary'],
+        });
+        this.router.navigate(['etudiant-list']);
+      }
+    );
+  }
+
+  createForm(etudiant?: Etudiant): FormGroup {
+    return this.fb.group({
+      nomControl: [etudiant.nom, [Validators.required]],
+      prenomControl: [etudiant.prenom, [Validators.required]],
+      usernameControl: [etudiant.username, [Validators.required]],
+      passwordControl: [etudiant.password, [Validators.required]],
+    });
+  }
+
+  submitEtudiant(): void {
+    if (this.etudiantForm.valid) {
+      const inputValues = this.etudiantForm.getRawValue();
+      const etudiant: Etudiant = {
+        id: this.etudiant.id,
+        nom: inputValues.nomControl,
+        prenom: inputValues.prenomControl,
+        username: inputValues.usernameControl,
+        password: inputValues.passwordControl,
+      };
+      this.etudiantService.updateEtudiant(etudiant).subscribe((data) => {
+        if (data) {
+          this.snackBar.open('Etudiant modifié avec succès', 'OK', {
+            duration: 5000,
+            panelClass: ['mat-toolbar', 'mat-primary'],
+          });
+          this.router.navigate(['secure/etudiant/list-etudiant']);
+        }
+      });
+    } else {
+      this.snackBar.open('Formulaire invalid', 'OK', {
+        duration: 3500,
+        panelClass: ['mat-toolbar', 'mat-primary'],
+      });
+    }
+  }
+
+  onBack(): void {
+    this.location.back();
+  }
+
+}
